2f164882a00371c43b0eba74be6387a02dd0275c
NHclub <sbe4488@naver.com>
2023-06-18 19:45:18 +0900

    algorithm

---
+++ src/_06_18/Problem26.java
@@ -0,0 +1,39 @@
+package _06_18;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Problem26 {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        }
+        int[] answer = new int[arr.length - 1];
+        List<Integer> lsArr = new ArrayList<>();
+        int index = 0;
+        for (int i : arr) {
+            lsArr.add(i);
+        }
+        Collections.sort(lsArr);
+        int Min = lsArr.get(0);
+        for (int i = 0; i < arr.length; i++) {
+            if (Min == arr[i]){
+                continue;
+            }else{
+                answer[index] = arr[i];
+                index++;
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem26 problem26 = new Problem26();
+        int[] arr = {10,4,8,9};
+        problem26.solution(arr);
+    }
+}

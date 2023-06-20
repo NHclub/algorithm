732ae7cb7bb9c90bad4524e1c3de5d37134d9a60
NHclub <sbe4488@naver.com>
2023-06-20 18:27:09 +0900

    algorithm

--- src/_06_20/Problem31.java
+++ src/_06_20/Problem31.java
@@ -2,33 +2,25 @@ package _06_20;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Problem31 {
     public int[] solution(int []arr) {
         Deque<Integer> number = new ArrayDeque<>();
         number.add(arr[0]);
-        for (int i = 0; i < arr.length-1; i++) {
-            if(number.pollLast() != arr[i]){
+
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i-1] != arr[i]){
                 number.add(arr[i]);
             }
         }
+
         int[] answer = new int[number.size()];
-        for(int i=0; i< number.size(); i++){
-            answer[i] = number.peek();
 
+        for(int i=0; i< answer.length; i++){
+            answer[i] = number.pollFirst();
         }
-
-
 
         return answer;
     }
 
-    public static void main(String[] args) {
-        Problem31 problem31 = new Problem31();
-        int[] num = {1, 1, 3, 3, 0, 1, 1};
-        problem31.solution(num);
-
-    }
 }
